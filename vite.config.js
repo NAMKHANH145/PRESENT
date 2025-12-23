882226374772a3783401cdf2191880a61360348d
Nam Khanh <namkhanh@example.com>
2025-12-23 18:06:27 +0000

    Update project content and deploy config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/PRESENT/', // GitHub Pages subdirectory
   plugins: [react()],
   server: {
     port: 3000,
